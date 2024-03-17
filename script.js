bb8cb7132784a9c41e764dc4a324d07e74dc5182
Over1Cloud <48094576+Over1Cloud@users.noreply.github.com>
2024-03-17 19:08:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+let pin = '';
+
+function addDigit(digit) {
+  if (pin.length < 4) {
+    pin += digit;
+    updatePinDisplay();
+    if (pin.length === 4) {
+      checkPIN();
+    }
+  }
+}
+
+function updatePinDisplay() {
+  const pinDigits = document.querySelectorAll('.pinDigit');
+  pinDigits.forEach((digit, index) => {
+    if (index < pin.length) {
+      digit.classList.add('filled');
+    } else {
+      digit.classList.remove('filled');
+    }
+  });
+}
+
+function checkPIN() {
+  if (pin === '7878') {
+    window.location.href = 'https://over1cloud.github.io/mira.github.io/';
+  } else {
+    shakeDigits(); // Добавляем вызов функции для тряски кружков
+    pin = '';
+    updatePinDisplay();
+  }
+}
+
+function shakeDigits() {
+  const pinDigits = document.querySelectorAll('.pinDigit');
+  pinDigits.forEach(digit => {
+    digit.classList.add('shake'); // Добавляем класс shake
+    setTimeout(() => {
+      digit.classList.remove('shake');
+    }, 500);
+  });
+}
+
